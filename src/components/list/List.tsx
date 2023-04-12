7b3d9cd495afcca3f50a0621d8f303e0aee5ba6c
just2click-pro <just2click@gmail.com>
2023-04-12 20:43:21 +0300

    [RND-1] Moved ListCell and ListHeader into separate components

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -2,25 +2,19 @@ import React, { FC, useState, useEffect } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux'
 
-import Box from '@mui/material/Box'
-import IconButton from '@mui/material/IconButton'
-import Paper from '@mui/material/Paper'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import Typography from '@mui/material/Typography'
-
 import { useTheme } from '@mui/material/styles'
-
-import { Edit, Delete, Add } from '@mui/icons-material'
 
 import { getEntityData } from '@/services/GetData'
 import { IEntitiesInfo } from '@/assets/data/entities'
 import DynamicSvgIcon from '@/components/dynamicSvgIcon/DynamicSvgIcon'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import Typography from '@mui/material/Typography'
 
-import { capitalizeFirstLetter } from '@/utils'
+import ListHeader from './ListHeader'
+import ListCell from './ListCell'
 
 import '@/assets/styles/list.css'
 
@@ -39,23 +33,6 @@ const List: FC<{ entityInfo: IEntitiesInfo }> = ({ entityInfo }) => {
 
     fetchEntityData()
   }, [])
-
-  const formatDifferentValues = (value: string): string => {
-    if (!value) return ''
-    const isNumeric = /^\d+$/.test(value)
-
-    if (isNumeric) {
-      return parseInt(value).toLocaleString('en-US')
-    }
-
-    return capitalizeFirstLetter(value)
-  }
-
-  const formatHeader = (value: string): string => {
-    if (!value) return ''
-    const withSpaces = value.replaceAll('_', ' ')
-    return capitalizeFirstLetter(withSpaces)
-  }
 
   return (
     <Box>
@@ -77,36 +54,11 @@ const List: FC<{ entityInfo: IEntitiesInfo }> = ({ entityInfo }) => {
         </Box>
 
         <Table className='list-table'>
-          <TableHead>
-            <TableRow>
-              {entityInfo.attributes.map((header: string) => (
-                <TableCell key={header} className='list-table-header-cell'>
-                  {formatHeader(header)}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
+          <ListHeader attributes={entityInfo.attributes} />
         </Table>
         <TableBody>
           {data.map((data: any) => (
-            <TableRow key={data.id}>
-              {entityInfo.attributes.map((header: string) => (
-                <TableCell key={header} className='list-table-cell'>
-                  {formatDifferentValues(data[header])}
-                </TableCell>
-              ))}
-              <TableCell>
-                <IconButton className='list-table-icon'>
-                  <Edit />
-                </IconButton>
-                <IconButton className='list-table-icon'>
-                  <Add />
-                </IconButton>
-                <IconButton className='list-table-icon'>
-                  <Delete />
-                </IconButton>
-              </TableCell>
-            </TableRow>
+            <ListCell data={data} attributes={entityInfo.attributes} />
           ))}
         </TableBody>
       </Paper>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,24 @@ export function capitalizeFirstLetter(str: string): string {
   const firstLetter = str.charAt(0)
   return firstLetter.toUpperCase() + str.slice(1)
 }
+
+export function convertUnderscoresToSpaces(value: string): string {
+  if (!value) return ''
+  const withSpaces = value.replaceAll('_', ' ')
+  return capitalizeFirstLetter(withSpaces)
+}
+
+function formatNumbers(value: string): string {
+  return parseInt(value).toLocaleString('en-US')
+}
+
+export function formatTableValues(value: string): string {
+  if (!value) return ''
+  const isNumeric = /^\d+$/.test(value)
+
+  if (isNumeric) {
+    return formatNumbers(value)
+  }
+
+  return capitalizeFirstLetter(value)
+}
